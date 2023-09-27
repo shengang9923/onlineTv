57edd821533c1b5379ea9fbcd59de30c3957f1b2
Maringmei <mshengangkabui@gmail.com>
2023-09-27 09:45:37 +0530

    ogin

--- lib/Screen/LoginPage/OtpLoginPage.dart
+++ lib/Screen/LoginPage/OtpLoginPage.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:onlinetv/Screen/Home/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OTPLoginPage extends StatefulWidget {
   @override
@@ -8,12 +10,10 @@ class OTPLoginPage extends StatefulWidget {
 }
 
 class _OTPLoginPageState extends State<OTPLoginPage> {
-
   TextEditingController otpController = TextEditingController();
   TextEditingController _phoneNumberController = TextEditingController();
   bool _isOTPSend = false;
   String vCode = '';
-
 
 // Function to send OTP to a phone number
   Future<void> sendOTP(String phoneNumber) async {
@@ -32,11 +32,10 @@ class _OTPLoginPageState extends State<OTPLoginPage> {
         codeSent: (String verificationId, int? resendToken) {
           // Store the verification ID somewhere to use it in the next step
           vCode = verificationId;
-          print('Verification ID: $verificationId');
-          _isOTPSend = true;
-          setState(() {
 
-          });
+          _isOTPSend = true;
+          EasyLoading.dismiss();
+          setState(() {});
           // You can also store resendToken for resending the OTP
         },
         codeAutoRetrievalTimeout: (String verificationId) {
@@ -50,18 +49,21 @@ class _OTPLoginPageState extends State<OTPLoginPage> {
     }
   }
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    pageRoute();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('OTP Login'),
-      ),
       body: Center(
         child: Column(
           children: [
-            if(_isOTPSend = false)
-            Padding(
+            if (_isOTPSend == false)
+              Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -71,7 +73,7 @@ class _OTPLoginPageState extends State<OTPLoginPage> {
                       style: TextStyle(fontSize: 18),
                     ),
                     SizedBox(height: 10),
-                    TextField(
+                    TextFormField(
                       controller: _phoneNumberController,
                       keyboardType: TextInputType.phone,
                       decoration: InputDecoration(
@@ -82,64 +84,92 @@ class _OTPLoginPageState extends State<OTPLoginPage> {
                     SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
-                        // Handle phone number submission here
                         String phoneNumber = _phoneNumberController.text;
-                        // Send the phone number for OTP verification
-                        sendOTP(phoneNumber);
+                        if (phoneNumber != '') {
+                          EasyLoading.show(status: "Sending OTP...");
+                          sendOTP("+91" + phoneNumber).then((value) {});
+                        }
                       },
                       child: Text('Submit'),
                     ),
                   ],
                 ),
-            ),
-            if(_isOTPSend = true)
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Enter OTP:',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  controller: otpController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: 'Enter OTP',
+              ),
+            if (_isOTPSend == true)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Enter OTP:',
+                    style: TextStyle(fontSize: 18),
                   ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    try {
-                      // Replace 'verificationId' with the actual verification ID obtained during OTP sent
-                      String verificationId = 'your_verification_id_here';
+                  SizedBox(height: 10),
+                  TextFormField(
+                    controller: otpController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: 'Enter OTP',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      EasyLoading.show(status: "Verifying OTP");
+                      try {
+                        // Replace 'verificationId' with the actual verification ID obtained during OTP sent
+                        String verificationId = '$vCode';
 
-                      AuthCredential credential = PhoneAuthProvider.credential(
-                        verificationId: verificationId,
-                        smsCode: otpController.text,
-                      );
+                        AuthCredential credential =
+                            PhoneAuthProvider.credential(
+                          verificationId: verificationId,
+                          smsCode: otpController.text,
+                        );
 
-                      await FirebaseAuth.instance.signInWithCredential(credential);
+                        await FirebaseAuth.instance
+                            .signInWithCredential(credential)
+                            .then((value) {
+                          setToken(_phoneNumberController.text);
+                          EasyLoading.dismiss();
+                        });
 
-                      // OTP verification successful, navigate to the next screen
-                      // Replace 'HomeScreen' with the screen you want to navigate to
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Home()),
-                      );
-                    } catch (e) {
-                      // Handle OTP verification failure
-                      print('Error verifying OTP: $e');
-                    }
-                  },
-                  child: Text('Verify OTP'),
-                ),
-              ],
-            ),
+                        // OTP verification successful, navigate to the next screen
+                        // Replace 'HomeScreen' with the screen you want to navigate to
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Home()),
+                        );
+                      } catch (e) {
+                        // Handle OTP verification failure
+                        print('Error verifying OTP: $e');
+                      }
+                    },
+                    child: Text('Verify OTP'),
+                  ),
+                ],
+              ),
           ],
         ),
       ),
     );
+  }
+
+  void setToken(phone) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString("TOKEN", phone);
+  }
+
+  Future<bool> getToken() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    if (sharedPreferences.getString("TOKEN") != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void pageRoute() async {
+    if (await getToken()) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:onlinetv/Screen/LoginPage/OtpLoginPage.dart';
 import 'package:provider/provider.dart';
 
 import 'Providers/app_provider.dart';
 import 'Screen/Home/home.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
     create: (BuildContext context) => AppProvider(),
     child: MyApp(),
@@ -23,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: Home(),
+      home: OTPLoginPage(),
       builder: EasyLoading.init(),
     );
   }
